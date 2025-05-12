98e1434f69076234424919457747f0834a407fdb
Samantha Ernano <samantha.ernano@gmail.com>
2025-05-12 17:00:02 -0400

    Install Postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'official_django_tutorial',
+    },
 }
 
 
