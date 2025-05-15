d2f4e782d90913059df98e128fd5c9431fc16d33
Samantha Ernano <samantha.ernano@gmail.com>
2025-05-15 18:00:18 -0400

    Fix polls index view

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     output = ", ".join([q.question_text for q in latest_question_list])
 
-    HttpResponse(output)
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
