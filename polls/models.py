d5660511da3fce173e35f01300576d80c273a9aa
Samantha Ernano <samantha.ernano@gmail.com>
2025-05-23 14:20:42 -0400

    More question admin form improvements

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -7,13 +8,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date_published")
 
-    def __str__(self):
-        return self.question_text
-
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
 
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
